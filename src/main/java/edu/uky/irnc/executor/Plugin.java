7861b8400559f49b03556cc36cf3b2c7e093b8bf
Caylin Hickey <caylin.hickey@uky.edu>
2016-08-30 13:51:14 -0400

    Testing

--- src/main/java/edu/uky/irnc/executor/Plugin.java
+++ src/main/java/edu/uky/irnc/executor/Plugin.java
@@ -18,6 +18,7 @@ public class Plugin extends CPlugin {
         String runCommand = config.getStringParam("runCommand");
         String dstPlugin = config.getStringParam("dstPlugin");
         String requiresSudo = config.getStringParam("sudo", "true");
+        logger.info("sudo = {}", requiresSudo);
         exchangeID = runCommand.substring(runCommand.lastIndexOf(" ") + 1);
         executeCommand(runCommand, dstPlugin, requiresSudo);
     }
