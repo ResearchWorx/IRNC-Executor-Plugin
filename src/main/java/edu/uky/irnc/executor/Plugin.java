ea6748851b9ee76689d3eef36defe27ab5b68af5
codybum <codeman01>
2017-10-25 09:19:42 -0400

    updated shared libs

--- src/main/java/edu/uky/irnc/executor/Plugin.java
+++ src/main/java/edu/uky/irnc/executor/Plugin.java
@@ -35,7 +35,7 @@ public class Plugin extends CPlugin {
 
     private static class Runner implements Runnable {
         private static Set<String> executables = new HashSet<>(Arrays.asList("netflow", "packet_trace",
-                "packet_validation", "sendudp", "kanon", "perfSONAR_Throughput", "amis_argus", "ls"));
+                "packet_validation", "sendudp", "kanon", "perfSONAR_Throughput", "amis_argus", "ls", "/home/yan/AMIS/amis"));
         private Plugin plugin;
         private CLogger logger;
         private String command;
